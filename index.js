b3bccc79e592e08731acddaa7954cdd86194db9c
Jacob Kriese <jkriese12@gmail.com>
2021-01-17 09:46:25 -0700

    Added starter files from hw folder

---
+++ index.js
@@ -0,0 +1,11 @@
+// array of questions for user p
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
