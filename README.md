51671e2c8a2859c2daaadc211354e84c5a38ed91
Jacob Kriese <jkriese12@gmail.com>
2021-01-17 14:59:55 -0700

    All sections added and user input guidelines

--- README.md
+++ README.md
@@ -1,30 +1,36 @@
-# lkdf
+# djfidjfidjfijdfidjfidjdf
 
-  ## Table of Contents
-  1. [Project Description](#Project-Description)
-  1. [Usage Information](#Usage-Information)
-  1. [Contribution](#Contribution)
-  1. [Installation Instructions](#Installation-Instructions)
-  1. [License](#License)
+## Table of Contents
 
-  ### Project Description
-    
-  ldkf
-  
-  ![image](https://www.placecage.com/600/300)
-  
-  ### Usage Information
-    
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    
-  ### Contribution
-    
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    
-  ### Installation Instructions
-    
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    
-  ### License
-    
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+1. [Project Description](#Project-Description)
+1. [Usage Information](#Usage-Information)
+1. [Contribution Guidelines](#Contribution-Guidelines)
+1. [Installation Instructions](#Installation-Instructions)
+1. [Test Instructions](#Test-Instructions)
+1. [License](#License)
+
+### Project Description
+
+dijfidjfidjkf djfidjd djfidjf kjfodkf dofkjodf dofkd f
+
+![image](https://www.placecage.com/600/300)
+
+### Usage Information
+
+dfkodfkdokosdosds
+
+### Contribution Guidelines
+
+odkfodkfwe0woe0woe
+
+### Installation Instructions
+
+qwerty
+
+### Test Instructions
+
+oskoiwieowew
+
+### License
+
+That other guy
--- index.js
+++ index.js
@@ -24,15 +24,42 @@ inquirer
       message: "Please provide a description of your project",
       name: "description",
     },
+    {
+      type: "input",
+      message: "Please provide usage information for your project",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Please provide contribution guidelines",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Please provide installation instructions",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Please provide test instructions",
+      name: "test",
+    },
+    {
+      type: "list",
+      message: "Which license was used for this project?",
+      name: "license",
+      choices: ["MIT", "Another one", "That other guy", "Or maybe this one"],
+    },
   ])
   .then((response) => {
-    const readMe = `# ${response.title}
+    const readMe = () => `# ${response.title}
 
   ## Table of Contents
   1. [Project Description](#Project-Description)
   1. [Usage Information](#Usage-Information)
-  1. [Contribution](#Contribution)
+  1. [Contribution Guidelines](#Contribution-Guidelines)
   1. [Installation Instructions](#Installation-Instructions)
+  1. [Test Instructions](#Test-Instructions)
   1. [License](#License)
 
   ### Project Description
@@ -43,21 +70,25 @@ inquirer
   
   ### Usage Information
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  ${response.usage}
     
-  ### Contribution
+  ### Contribution Guidelines
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  ${response.contribution}
     
   ### Installation Instructions
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  ${response.installation}
+
+  ### Test Instructions
+
+  ${response.test}
     
   ### License
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+  ${response.license}`;
 
-    fs.writeFile("README.md", readMe, (err) =>
+    fs.writeFile("README.md", readMe(), (err) =>
       err ? console.error(err) : console.log("commit logged!")
     );
   });
