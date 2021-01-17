63026d996108c2e52637128bcb16456bea9dfcde
Jacob Kriese <jkriese12@gmail.com>
2021-01-17 11:32:08 -0700

    Updates to inquirer prompts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# README Generator
+# Node ReadME Generator
 
   ## Table of Contents
   1. [Project Description](#Project-Description)
@@ -7,9 +7,9 @@
   1. [Installation Instructions](#Installation-Instructions)
   1. [License](#License)
 
-  ### dlokf
+  ### Project Description
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  Command line program that takes in user data to create a dynamically generated readme file for the user
     
   ### Usage Information
     
--- index.js
+++ index.js
@@ -21,27 +21,12 @@ inquirer
     },
     {
       type: "input",
-      message: "Where are you located?",
-      name: "location",
-    },
-    {
-      type: "input",
-      message: "Write a brief bio of yourself.",
-      name: "bio",
-    },
-    {
-      type: "input",
-      message: "What is your LinkedIn URL?",
-      name: "link",
-    },
-    {
-      type: "input",
-      message: "What is your Github URL?",
-      name: "git",
+      message: "Please provide a description of your project",
+      name: "description",
     },
   ])
   .then((response) => {
-    const readMe = `# README Generator
+    const readMe = `# ${response.title}
 
   ## Table of Contents
   1. [Project Description](#Project-Description)
@@ -50,9 +35,9 @@ inquirer
   1. [Installation Instructions](#Installation-Instructions)
   1. [License](#License)
 
-  ### ${response.title}
+  ### Project Description
     
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  ${response.description}
     
   ### Usage Information
     
